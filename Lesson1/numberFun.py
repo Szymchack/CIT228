f340cae5d89eba4b94494c404182c3ae80916858
CathyAnn Szymchack <szymchc@mail.nmc.edu>
2021-01-18 12:57:31 -0500

    Initial Commit

---
+++ Lesson1/numberFun.py
@@ -0,0 +1,25 @@
+import random
+number1 = random.randrange(100)
+number2 = random.randrange(100)
+print(number1)
+print(number2)
+print("Number 1 = 67")
+print("Number 2 = 8")
+x=67
+y=8
+print(x//y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(x+y)
+print(x-y)
+print(x*y)
+
+NUMBER2 = 25
+print(x//NUMBER2)
+print(x/NUMBER2)
+print(x%NUMBER2)
+print(x**NUMBER2)
+print(x+NUMBER2)
+print(x-NUMBER2)
+print(x*NUMBER2)
---
+++ Lesson1/stringFun.py
@@ -0,0 +1,54 @@
+print("-------------------------------------------")
+print("Exercise 1")
+name="cathyann szymchack"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("My First initial is:", name[0].upper())
+
+print("-------------------------------------------")
+print("Exercise 2")
+noun = "frog"
+adj = "happy"
+verb = "sang"
+ending = "all through the night"
+sentence1 = "the " + adj + " green " +  noun +" " + verb + " " + ending
+sentence2 = f"the {adj} green {noun} {verb} {ending}"
+print(sentence1.upper())
+print(sentence2.lower())
+
+print("--------------------------------------------")
+print("Exercise 3")
+beginning = "my dearest child"
+noun = " you"
+verb = " will go"
+pronoun = "your"
+ending = " even your wildest dreams and expectations!"
+sentence1 = beginning + noun +  verb + " beyond:"
+sentence2 = pronoun + " last war and victory,"
+sentence3 = pronoun + " last level of strength,"
+sentence4 = pronoun + " last level of victory,"
+sentence5 = ending
+print (sentence1.title())
+print ("\t", sentence2)
+print ("\t", sentence3)
+print ("\t", sentence4)
+print (sentence5)
+
+print("----------------------------------------------")
+print("Exercise 4")
+string1 = "Movie"
+string2 = "TV show"
+value1 = "Red"
+value2 = "Survivor"
+value3 = "Alaskan Bush People"
+value4 = "Olympus Has Fallen"
+value5 = "Calamity Jane"
+value6 = "Expedition Unknown"
+print(string1,"\t\t",string2,"\n")
+print(value1,"\n")
+print("\t\t",value2)
+print("\t\t",value3)
+print(value4,"\n")
+print(value5,"\n")
+print("\t\t", value6)
