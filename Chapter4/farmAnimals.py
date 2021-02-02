d640c87c9c5fb04be2049918203e7b3d51dc1fbd
Szymchack <44557214+Szymchack@users.noreply.github.com>
2021-02-02 13:10:39 -0500

    Add files via upload

--- Chapter4/farmAnimals.py
+++ Chapter4/farmAnimals.py
@@ -18,15 +18,3 @@ for animal in newAnimals:
 print("The first 3 animals in the new list are:", newAnimals[0:3])
 print("The middle 3 animals are:", newAnimals[3:6])
 print("The last 3 animals are:", newAnimals[6:10])
-print("---------------4-13---------------------")
-buffetFoods=("chicken","meatballs","spegetti","salad")
-for foods in buffetFoods:
-    print("One type of food is", foods)
-print("-------------error on purpose-----------")
-
-print("-------------how to change menu---------")
-buffetFoods=("chicken", "pot roast", "quesadilla", "pizza")
-for foods in buffetFoods:
-    print("January Menu changes", foods)
-
-
