9553af392ac7610221e275cd08042c234112cae1
CathyAnn Szymchack <szymchc@mail.nmc.edu>
2021-02-04 12:21:55 -0500

    Lesson 5

---
+++ Chapter5/hello_admin.py
@@ -0,0 +1,6 @@
+usernames = ['Hadassa', 'Erin', 'Ezra', 'Admin', 'Peter']
+for username in usernames:
+    if username == 'Admin':
+        print("Welcome Admin, would you like to see the user report for today?")
+    else:
+        print(f"Welcome {username}, thank you for consistance in logging on.")
